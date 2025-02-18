8dada52f7dbfc564a54053babd29bfc563fd7732
tituscarl <titusranario@gmail.com>
2025-02-18 15:35:06 +0800

    test health check

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"net/http"
 )
 
 func main() {
@@ -13,13 +14,26 @@ func main() {
 	defer listener.Close()
 
 	log.Println("TCP server listening on :80")
-	for {
-		_, err := listener.Accept()
-		if err != nil {
-			log.Printf("Error accepting connection: %s", err)
-			continue
-		}
 
-		log.Println("Accepted connection")
+	go func() {
+		for {
+			c, err := listener.Accept()
+			if err != nil {
+				log.Printf("Error accepting connection: %s", err)
+				continue
+			}
+			log.Println("Accepted connection for health checks")
+			c.Close()
+		}
+	}()
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Received!")
+		w.WriteHeader(http.StatusOK)
+	})
+
+	err = http.ListenAndServe(":50051", nil)
+	if err != nil {
+		log.Fatalf("Error starting HTTP server: %s", err)
 	}
 }
